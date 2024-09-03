e666e88c9c592f84bd27b004076a29542f685dd4
Emisael Kisler <emisael@localhost.localdomain>
2024-09-03 18:30:22 -0400

    part12-7 OK

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -1,49 +1,77 @@
-const express = require('express');
-const { Todo } = require('../mongo')
+const express = require("express");
+const { Todo } = require("../mongo");
 const router = express.Router();
 
+const findByIdMiddleware = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    req.todo = await Todo.findById(id);
+
+    if (!req.todo) {
+      return res.sendStatus(404);
+    }
+    next();
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+};
+
 /* GET todos listing. */
-router.get('/', async (_, res) => {
-  const todos = await Todo.find({})
+router.get("/", async (_, res) => {
+  const todos = await Todo.find({});
   res.send(todos);
 });
 
 /* POST todo to listing. */
-router.post('/', async (req, res) => {
+router.post("/", async (req, res) => {
   const todo = await Todo.create({
     text: req.body.text,
-    done: false
-  })
+    done: false,
+  });
   res.send(todo);
 });
 
-const singleRouter = express.Router();
+/* GET todo. */
+router.get("/:id", findByIdMiddleware, async (req, res) => {
+  if (!req.todo) {
+    return res.status(404).json({ message: "Todo not found" });
+  }
 
-const findByIdMiddleware = async (req, res, next) => {
-  const { id } = req.params
-  req.todo = await Todo.findById(id)
-  if (!req.todo) return res.sendStatus(404)
-
-  next()
-}
+  res.json(req.todo);
+});
 
 /* DELETE todo. */
-singleRouter.delete('/', async (req, res) => {
-  await req.todo.delete()  
+router.delete("/", async (req, res) => {
+  await req.todo.delete();
   res.sendStatus(200);
 });
 
-/* GET todo. */
-singleRouter.get('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
-});
-
 /* PUT todo. */
-singleRouter.put('/', async (req, res) => {
-  res.sendStatus(405); // Implement this
+router.put("/:id", findByIdMiddleware, async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updateData = req.body;
+
+    if (String(id) !== String(req.todo.id)) {
+      return res.status(403).json({ message: "Not autorized" });
+    }
+
+    // Actualizar el todo
+    const updatedTodo = await Todo.findByIdAndUpdate(id, updateData, {
+      new: true,
+    });
+
+    if (!updatedTodo) {
+      return res.status(404).json({
+        message: "Todo not find",
+      });
+    }
+
+    res.json(updatedTodo);
+  } catch (error) {
+    console.error("Error to update todo:", error);
+    res.status(500).json({ error: "Error to update todo" });
+  }
 });
-
-router.use('/:id', findByIdMiddleware, singleRouter)
-
 
 module.exports = router;
