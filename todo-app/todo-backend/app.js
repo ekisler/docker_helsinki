72044752abd0a0ec8575808a007ed577793d2892
Emisael Kisler <kisler.fullstack@gmail.com>
2024-09-04 13:53:47 -0400

    part12-9 OK

--- todo-app/todo-backend/app.js
+++ todo-app/todo-backend/app.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const logger = require('morgan');
 const cors = require('cors');
+const redis = require('redis');
 
 const indexRouter = require('./routes/index');
 const todosRouter = require('./routes/todos');
 
 const app = express();
 
-app.use(cors());
+const redisClient = redis.createClient({
+    url: process.env.REDIS_URL
+})
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 
